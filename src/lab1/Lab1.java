15d93ae52b1496f368635b3447a22c99ba2fd18b
kubakrymarys <kuba.krymarys@gmail.com>
2017-10-08 18:17:46 +0200

    inital commit

---
+++ src/lab1/Lab1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author krymu
+ */
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter PESEL");
+        String s = br.readLine();
+        System.out.println(s);
+        
+    }
+
+}
